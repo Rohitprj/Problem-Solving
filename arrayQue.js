d0b987416e178b17c00247d78b23eae1a3f146e9
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-18 16:49:29 +0530

    Add setTimeoutQue.js for asynchronous logging and update arrayQue.js with console logging examples

--- arrayQue.js
+++ arrayQue.js
@@ -1,8 +1,14 @@
 // find prime number
 // arrange prime number in assending order and print
 // i % 5 == 0
+
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(arr);
-// for (let i = 0; i <= arr.length; i++) {
-//   console.log(i);
+// console.log(arr);
+// const newArr = [];
+// if (arr === arr % 1 == 0  || arr === arr % arr == 0) {
+//   arr.push(newArr);
+//   console.log(newArr);
 // }
+for (let i = 0; i <= arr.length; i++) {
+  console.log(i);
+}
