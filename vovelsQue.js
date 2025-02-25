2b7e9458396cc09593287849d36dd54b67b6f993
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-25 13:27:31 +0530

    Refactor vovelsQue.js to use regex for vowel checking and update test cases

--- vovelsQue.js
+++ vovelsQue.js
@@ -1,9 +1,10 @@
 // WAP to check vovels in a word or sentence ?
 // A E I O U
+
 function checkVovls(word) {
-  const check = word.split("");
-  if (condition) {
-  }
+  const check = word.match(/[aeiouAEIOU]/);
   console.log(check);
+  //   return check ? check.length : 0;
 }
-checkVovls("Rohit");
+checkVovls("Name");
+// console.log(checkVovls("Rohit"));
