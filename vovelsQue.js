f7e95fc05e8eb03109f342a8f0f637d09062358f
Rohit kumar <rohitprj.1282@gmail.com>
2025-02-25 13:09:27 +0530

    Add vovelsQue.js to check for vowels in a word or sentence

---
+++ vovelsQue.js
@@ -0,0 +1,9 @@
+// WAP to check vovels in a word or sentence ?
+// A E I O U
+function checkVovls(word) {
+  const check = word.split("");
+  if (condition) {
+  }
+  console.log(check);
+}
+checkVovls("Rohit");
