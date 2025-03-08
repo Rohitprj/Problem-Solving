51d98d268ed1c59a1465ad386878ae46d99b3242
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-08 17:07:53 +0530

    Implement interval-based value increment in index component

--- methodPr/app/index.jsx
+++ methodPr/app/index.jsx
@@ -1,13 +1,25 @@
 import { View, Text } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function index() {
-  const [value, setValue] = useState(10);
-  const interval = () => {};
-  setInterval(interval, 1000);
+  const [value, setValue] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setValue((preValue) => {
+        if (preValue >= 10) {
+          clearInterval(interval);
+          return preValue;
+        }
+        return preValue + 1;
+      });
+    }, 1000);
+    // return () => {
+    //   clearInterval(interval);
+    // };
+  }, []);
   return (
     <View>
-      <Text>index{value}</Text>
+      <Text>index {value}</Text>
     </View>
   );
 }
