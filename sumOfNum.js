556adbbffcff6f6d8087befe3c33871c71812672
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-09 19:59:03 +0530

    Add error handling for negative numbers in sumOfNum function

--- sumOfNum.js
+++ sumOfNum.js
@@ -1,11 +1,13 @@
 function sumOfNum(num) {
+  if (num < 0) {
+    throw new Error("It only takes num greater than one");
+  }
   let s = 0;
   const b = num.toString();
   const c = b.split("");
-  console.log(c);
   c.forEach((e) => {
     s = s + parseInt(e);
   });
   console.log(s);
 }
-sumOfNum(12);
+sumOfNum(18);
