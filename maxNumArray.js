2b9821ca56a07910c209f7019a24703e1ea09987
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-15 17:31:20 +0530

    Refactor maxNum function to use Math.max for finding the largest number and remove input array handling

--- maxNumArray.js
+++ maxNumArray.js
@@ -1,13 +1,20 @@
-function maxNum(arr) {
-  if (arr.length < 1) {
-    throw new Error("Empty array !");
-  }
-  let biggestNum = arr[0];
-  for (let i = 1; i <= arr.length; i++) {
-    if (arr[i] > biggestNum) {
-      biggestNum = arr[i];
-    }
-  }
-  console.log(arr, biggestNum);
+// function maxNum(arr) {
+//   if (arr.length < 1) {
+//     throw new Error("Empty array !");
+//   }
+//   let biggestNum = arr[0];
+//   for (let i = 1; i <= arr.length; i++) {
+//     if (arr[i] > biggestNum) {
+//       biggestNum = arr[i];
+//     }
+//   }
+//   console.log(arr, biggestNum);
+// }
+// maxNum([8, -2, -56, 12, 1]);
+
+// With inbuilt functions
+function maxNum() {
+  let biggestNum = Math.max(8, 10, -56, 12, 1);
+  console.log(biggestNum);
 }
-maxNum([8, -2, -56, 12, 1]);
+maxNum();
