8661d64773edb159848aea75edba6c06c57ba15d
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-08 21:14:24 +0530

    java notes

---
+++ java/ExceptionHandling/javaExceptions.java
@@ -0,0 +1,7 @@
+// Java Exceptions
+// Java Exception handling
+// try....catch block
+// finally block
+// throw and throws keyword
+
+// There are two types of exceptions in java runtime exceptions and java IOException
