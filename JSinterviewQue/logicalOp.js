dc3ab1d4a2a36caae8afb6a0753a86ad0e19ea8f
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-23 18:50:22 +0530

    Remove commented-out code and retain string reversal implementation

--- JSinterviewQue/logicalOp.js
+++ JSinterviewQue/logicalOp.js
@@ -11,7 +11,41 @@
 // // }
 
 // Que 2
-person = { name: "John", age: 30, city: "Delhi" };
-console.log(Object.keys(person));
-console.log(Object.values(person));
-console.log(Object.entries(person));
+// person = { name: "John", age: 30, city: "Delhi" };
+// console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+
+// Que 3
+// let date = new Date();
+// console.log(date);
+
+// Que 4
+// const person = new Object();
+// person.name = "Rohit";
+// person.age = "Alice";
+// console.log(person.age);
+
+// Que 5
+
+// function palindrome(name) {
+//   let j = name.length - 1;
+//   for (let i = 0; i < name.length / 2; i++) {
+//     if (name[i] != name[j]) {
+//       return false;
+//     }
+//     j--;
+//   }
+//   return true;
+// }
+// let result = palindrome("wow");
+// console.log(result);
+
+// Que 6
+// console.log(a.split("").reverse().join(""));
+let a = "Rohit";
+let b = "";
+for (let i = a.length - 1; i >= 0; i--) {
+  b = b + a[i];
+}
+console.log(b);
