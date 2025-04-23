9c868ec5f04b04b698a77724dcbfe81b405df5aa
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-23 19:09:53 +0530

    Update fetchData function to use new API endpoint with proper request options

--- fetchData.js
+++ fetchData.js
@@ -1,7 +1,12 @@
 async function getData() {
   try {
-    const uri = "https://jsonplaceholder.typicode.com/posts";
-    const response = await fetch(uri);
+    const uri = "https://backend.rohitkumar.site/categories/allCategories";
+    const response = await fetch(uri, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     const data = await response.json();
     console.log(data);
   } catch (error) {
