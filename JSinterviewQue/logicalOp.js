aad510214f01e804456cd9035ba9dc558f68cdf6
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-23 17:04:16 +0530

    Refactor logical operations and enhance object property logging

--- JSinterviewQue/logicalOp.js
+++ JSinterviewQue/logicalOp.js
@@ -1,8 +1,17 @@
-let a;
-let b;
+// Que 1
+// let a = 10;
+// let b = 10;
+// let c = a === 10 ? "yes" : "No";
+// console.log(c);
 
-if (a && b) {
-  console.log("Yes");
-} else {
-  console.log("No");
-}
+// // if (a || b) {
+// //   console.log("Yes");
+// // } else {
+// //   console.log("No");
+// // }
+
+// Que 2
+person = { name: "John", age: 30, city: "Delhi" };
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
