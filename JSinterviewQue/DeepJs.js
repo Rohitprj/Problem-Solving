0857fce78348e8edb0a9c25aca70a3a44437d457
Rohit kumar <rohitprj.1282@gmail.com>
2025-05-09 20:14:20 +0530

    Add JavaScript interview questions: temporal deadzone, event loop, scopes, and hoisting

---
+++ JSinterviewQue/DeepJs.js
@@ -0,0 +1,17 @@
+// React js interview
+
+// 1) temporal deadzone
+// 2) Event Loop
+// 3) This keyword
+// 4) Call bind apply
+// 5) Closure
+// 6) ?.
+// 7) ??
+// 8) CSR-SSR
+// 9) React
+// 10)React.memo vs useMemo
+// 11)Cleanup in useEffect
+// 12)UseCallback
+// 13)Call stack
+// 14)Typescript generics
+// 15)Type and interference in ts
---
+++ JSinterviewQue/Scopes.js
@@ -0,0 +1,2 @@
+// There are 3 types of scopes in js
+// Global - Function - Block
