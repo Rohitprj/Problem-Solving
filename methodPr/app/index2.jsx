6dfe61327a84a82a760edeca36c0e66601a25df7
Rohit kumar <rohitprj.1282@gmail.com>
2025-05-18 13:29:05 +0530

    Add index2 component: Create a new React Native component with basic structure

---
+++ methodPr/app/index2.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function index2() {
+  return (
+    <View>
+      <Text>index2</Text>
+    </View>
+  );
+}
