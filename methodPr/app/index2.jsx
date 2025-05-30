cdf597fd7daf36db46e3ee0ffe2acb553d9667b2
Rohit kumar <rohitprj.1282@gmail.com>
2025-05-30 17:51:37 +0530

    updates

---
+++ methodPr/app/index2.jsx
@@ -0,0 +1,27 @@
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+
+export default function index() {
+  const [timeStart, setTimeStart] = useState(6);
+  const [show, setShow] = useState(false);
+  function timer() {
+    useEffect(() => {
+      if (timeStart <= 0) {
+        setShow(true);
+        return;
+        //  clearInterval(time);
+      }
+      const time = setInterval(() => {
+        setTimeStart((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(time);
+    }, [timeStart]);
+  }
+  timer();
+  return (
+    <View>
+      <Text>Timer : {timeStart}</Text>
+      <Text>{show && <Text>Done</Text>}</Text>
+    </View>
+  );
+}
