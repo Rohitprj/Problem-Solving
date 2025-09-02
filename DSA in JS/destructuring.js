f5f4789497c3cb1a2936ffce07efcfffa5ad921f
Rohit kumar <rohitprj.1282@gmail.com>
2025-09-02 19:01:49 +0530

    DSA

---
+++ DSA in JS/destructuring.js
@@ -0,0 +1,18 @@
+// let a = 2,
+//   b = 3;
+
+// [a, b] = [b, a]; // Swapping values
+// console.log(a, b);
+
+let a = 11;
+let b = 3;
+
+// console.log(Math.floor(a / b));
+// console.log(a % b);
+// console.log(11 !== 3);
+
+// console.log(11 == "11"); //true
+// console.log(11 === "11"); // flase
+
+const c = a++;
+console.log(c);
