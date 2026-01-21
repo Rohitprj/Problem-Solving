6f485dd2ba1a99e8df892da561e04f646ef6969a
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-21 15:44:10 +0530

    Refactor IFFE implementation and add union type example in TypeScript

---
+++ TYPESCRIPT/basic/src/unionAndany.ts
@@ -0,0 +1,9 @@
+// Union example
+
+let folowers: string | number = "1m";
+
+let apiStatus: "pending" | "complete" | "error" = "pending";
+
+apiStatus = "complete"; // only accepts these 3
+
+// Avoide any as much as possible
