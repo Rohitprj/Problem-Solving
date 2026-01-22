b38c9d28635d4607c0d092da6e787f9bfa4d2a24
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-22 16:36:40 +0530

    notes

---
+++ JAVASCRIPT/basics/03_OperatorsAndConditionalStatmenst.js
@@ -0,0 +1,37 @@
+/* 
+
+-------------------OPERATORS---------------------------------
+
+JavaScript Arithmetic Operators
+Arithmetic operators perform arithmetic on numbers (literals or variables).
+
+Operator	    Description
+   +	        Addition
+   -	        Subtraction
+   *	        Multiplication
+   **	        Exponentiation (ES2016)
+   /	        Division
+   %	        Modulus (Remainder)
+   ++	        Increment
+   --	        Decrement
+
+*/
+
+/*
+
+JavaScript Assignment Operators
+Assignment operators assign values to JavaScript variables.
+
+Given that x = 10 and y = 5, the table below explains the assignment operators:
+
+Operator	Example	    Same As	      Result
+=	        x = y	    x = y	          x = 5
++=	        x += y	    x = x + y	      x = 15
+-=	        x -= y	    x = x - y	      x = 5
+*=	        x *= y	    x = x * y	      x = 50
+**=	        x **= y	    x = x ** y	      x = 100000
+/=	        x /= y	    x = x / y	      x = 2
+%=	        x %= y	    x = x % y	      x = 0
+:	        x: 45	    size.x = 45	      x = 45
+
+*/
