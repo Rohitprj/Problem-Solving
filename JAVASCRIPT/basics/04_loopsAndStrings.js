1ab1be4157e1d5b5757d1baa322b9e20befe6517
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-24 13:49:19 +0530

    Add initial documentation for loops in JavaScript

---
+++ JAVASCRIPT/basics/04_loopsAndStrings.js
@@ -0,0 +1,35 @@
+/*
+Loops in JS
+
+## for loop
+
+for (let i = 1; i <= 3; i++) {
+    console.log("Count:", i);
+}
+
+WAP to print 1 to 10
+WAP to print 10 to 1
+WAP to print sum of 1 to 10
+
+## Practice Set
+WAP to print 2 to 20
+WAP to print 20 to 2
+
+## while loop
+
+let i = 0;
+while (i < 3) {
+    console.log("Number:", i);
+    i++;
+}
+
+
+## do-while loop
+
+let i = 0;
+do {
+    console.log("Number:", i);
+    i++;
+} while (i < 3);
+
+*/
