78763870740f0f842377938aa83d1de778caa958
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-30 17:34:02 +0530

    Add form elements and improve video/audio structure in HTML files

--- JAVASCRIPT/basics/04_loopsAndStrings.js
+++ JAVASCRIPT/basics/04_loopsAndStrings.js
@@ -46,3 +46,44 @@ do {
 //   i++; // increment
 //   console.log(i);
 // } while (i < 3);
+
+/*
+
+PS
+
+WAP to print 1 to 10
+WAP to print 10 to 1
+WAP to print table of 2
+Make that table of 2 in this format ( 2 x 1 = 2, 2 X 2 = 4 )
+WAP to print odd num in 1 to 100
+WAP to print even num in 1 to 100
+
+*/
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/*
+
+Strings in JS
+
+create a string
+let name = "Asha learnlogy";
+
+string length
+name.length
+
+string indices
+name[0], name[1], name[2]
+
+Template literals ( In which we can add expressions and variables into our strings. In some other words we can say string Interpolation.)
+let specialString = `We are a student of web development in ${name}`
+
+Escape characterstices
+
+\n ( for next line )
+console.lo("Asha \n learnlogy")
+
+\t ( for tab space )
+console.log("Asha \t learnlogy")
+
+*/
