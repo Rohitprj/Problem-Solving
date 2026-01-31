7b190a55a138d370ab04d8562e8e345e2e98ace9
Rohit kumar <rohitprj.1282@gmail.com>
2026-01-31 17:23:54 +0530

    TS practice

--- TYPESCRIPT/basic/src/moreTypes.ts
+++ TYPESCRIPT/basic/src/moreTypes.ts
@@ -1,10 +1,10 @@
-// Type Assertion
+// Type Assertion-------------------------------------------------
 
 let response: any = "404";
 let numLength: number = (response as string).length;
 console.log(numLength);
 
-// Defined type of a response data
+// Defined type of a response data--------------------------------
 type User = {
   name: string;
   password: number;
@@ -14,9 +14,55 @@ let data = '{"name": "Rohit", "Password": 123456 }';
 let dataObj = JSON.parse(data) as User;
 console.log(dataObj);
 
+// With any-------------------------------------------------------
 let a: any;
 
-a = [1, 2, 3];
 a = "code";
+a = [1, 2, 3];
+a = 10;
 
 console.log(a.toUpperCase());
+
+// With unknown---------------------------------------------------
+let b: unknown;
+
+b = "code";
+b = [1, 2, 3];
+b = 10;
+
+if (typeof b === "string") {
+  b.toUpperCase();
+  console.log(b.toUpperCase());
+}
+
+// Type guarding --------------------------------------------------
+try {
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(error.message);
+  }
+  console.log(error);
+}
+
+// Type Assertion--------------------------------------------------
+const res: unknown = "Server is running";
+const strData: string = res as string;
+
+// Type never------------------------------------------------------
+
+type Role = "admin" | "user" | "superadmin";
+
+function assignRole(role: Role): void {
+  if (role === "admin") {
+    console.log("Redirect to admin dashboard");
+    return;
+  } else if (role === "user") {
+    console.log("Redirect to user dashboard");
+    return;
+  }
+  role;
+}
+
+function neverReturn(): never {
+  while (true) {}
+}
