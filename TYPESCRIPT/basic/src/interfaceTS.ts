6e8732e9b05da338e64ac01000b019ed3b63d470
Rohit kumar <rohitprj.1282@gmail.com>
2026-03-14 12:12:56 +0530

    Refactor interface example in interfaceTS.ts to improve clarity and organization

--- TYPESCRIPT/basic/src/interfaceTS.ts
+++ TYPESCRIPT/basic/src/interfaceTS.ts
@@ -1,3 +1,24 @@
-console.log("====================================");
-console.log("Hello world");
-console.log("====================================");
+/*
+1️⃣ Basic Example
+
+Interface
+
+interface User {
+  name: string;
+  age: number;
+}
+
+Type
+
+type User = {
+  name: string;
+  age: number;
+}
+
+Use:
+const user: User = {
+  name: "Rohit",
+  age: 22
+};
+
+*/
