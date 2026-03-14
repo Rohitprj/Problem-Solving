20d2ed214a4237ecd18e44810f78b0649960bcbf
Rohit kumar <rohitprj.1282@gmail.com>
2026-03-14 12:22:37 +0530

    Add examples for extending interfaces and merging in interfaceTS.ts

--- TYPESCRIPT/basic/src/interfaceTS.ts
+++ TYPESCRIPT/basic/src/interfaceTS.ts
@@ -22,3 +22,88 @@ const user: User = {
 };
 
 */
+
+/*
+
+2️⃣ Extend / Inheritance
+Interface me extend simple hota hai
+
+interface User {
+  name: string;
+}
+
+interface Admin extends User {
+  role: string;
+}
+
+Use:
+const admin: Admin = {
+  name: "Rohit",
+  role: "Admin"
+};
+
+Type me extend & se hota hai
+type User = {
+  name: string;
+}
+
+type Admin = User & {
+  role: string;
+}
+
+ */
+
+/*
+
+3️⃣ Interface Merge ho sakta hai
+
+Interface ko multiple times declare kar sakte ho.
+
+interface User {
+  name: string;
+}
+
+interface User {
+  age: number;
+}
+
+TypeScript automatically merge karega:
+
+const user: User = {
+  name: "Rohit",
+  age: 22
+};
+
+⚠️ Type me ye possible nahi hai
+
+type User = {
+  name: string;
+}
+
+type User = {   // ❌ Error
+  age: number;
+}
+
+*/
+
+/*
+
+4️⃣ Type me extra powers hoti hain
+
+Type unions bana sakta hai.
+
+type Status = "success" | "error" | "loading";
+
+Interface ye nahi kar sakta.
+
+*/
+
+/*
+🔑 Simple Rule (Industry)
+
+Mostly developers follow this rule:
+
+✔ Interface → Objects / API response
+✔ Type → Unions, primitives, complex types
+
+ */
