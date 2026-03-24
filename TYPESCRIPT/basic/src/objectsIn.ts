f4c7b410b9c3ffc96c2b2054fbc97a08c77dd326
Rohit kumar <rohitprj.1282@gmail.com>
2026-03-24 14:03:14 +0530

    "Added some content"

--- TYPESCRIPT/basic/src/objectsIn.ts
+++ TYPESCRIPT/basic/src/objectsIn.ts
@@ -44,7 +44,7 @@ type Chai = {
   isHot: boolean;
   howMany: number;
 };
-// Partial works with partial values
+// Partial works with partial or few values
 const updateChai = (updates: Partial<Chai>) => {
   console.log(`Preparing chai with ${updates}`);
 };
@@ -53,15 +53,44 @@ updateChai({ name: "chai" });
 updateChai({ isHot: true });
 updateChai({});
 
+//----------------------------------------------------------------------------------------------------------
+
 type User = {
   name?: string;
   password?: number;
 };
 
-// Requires all values if its types are optional
+// Requires, all values if its types are optional or not
 
 const register = (data: Required<User>) => {
   console.log(`user data ${data}`);
 };
 
 register({ name: "User", password: 123 });
+
+//----------------------------------------------------------------------------------------------------------
+
+type Profile = {
+  name: string;
+  age: number;
+  image: string;
+  isLoggedIn: boolean;
+};
+// Pick means pick only values that is written
+type ProfileInfo = Pick<Profile, "name" | "isLoggedIn">;
+
+const passInfo: ProfileInfo = {
+  name: "Rohit Kumar",
+  isLoggedIn: true,
+};
+
+//----------------------------------------------------------------------------------------------------------
+
+type NewProfile = {
+  name: string;
+  age: number;
+  image: string;
+  isLoggedIn: boolean;
+};
+// Omit means omit or leave values that is written
+type ProfileInfo2 = Omit<NewProfile, "image">;
