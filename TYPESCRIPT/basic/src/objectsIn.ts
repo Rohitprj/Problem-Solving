7147851becdd60b5e081dd47dd5fe93526ae5a09
Rohit kumar <rohitprj.1282@gmail.com>
2026-03-24 13:22:10 +0530

    Add TypeScript examples for object types and utility types

---
+++ TYPESCRIPT/basic/src/objectsIn.ts
@@ -0,0 +1,39 @@
+// let chai: {
+//   name: string;
+//   type: string;
+//   isHot: boolean;
+//   howMany: number;
+// };
+
+// chai = {
+//   name: "goodChai",
+//   type: "masala",
+//   isHot: true,
+//   howMany: 12,
+// };
+
+// type Coffee = {
+//   name: string;
+//   price: number;
+//   ingredients: string[];
+// };
+
+// const bnaoCoffee: Coffee = {
+//   name: "espresso",
+//   price: 200,
+//   ingredients: ["milk", "coffee", "whipCream"],
+// };
+
+//----------------------------------------------------------------------------------------------------------
+
+// We can add extra values :-
+
+type Nescafe = { time: number };
+
+const coffee = { time: 5, beans: "Arabica" };
+
+const makeCoffee: Nescafe = coffee;
+
+//----------------------------------------------------------------------------------------------------------
+
+// Utility Types in TS
