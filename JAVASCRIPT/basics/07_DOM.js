c82c7a9ec2359d58d6b2eccbeef0ab330313bddd
Rohit kumar <rohitprj.1282@gmail.com>
2026-03-25 17:29:54 +0530

    Add DOM manipulation examples and comments to 07_DOM.js

--- JAVASCRIPT/basics/07_DOM.js
+++ JAVASCRIPT/basics/07_DOM.js
@@ -86,4 +86,9 @@ Selecting with queryAll :- document.querySelectorAll(â€œ#myId / .myClass / tagâ€
 
 Properties
 
+tagName : returns tag for element nodes
+innerText : returns the text content of the element and all its children
+innerHTML : returns the plain text or HTML contents in the element
+textContent : returns textual content even for hidden elements
+
 */
