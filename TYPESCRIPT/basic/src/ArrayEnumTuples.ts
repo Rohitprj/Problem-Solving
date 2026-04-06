005177fa7d9c139dda1c543f605162c38910473e
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-06 18:36:32 +0530

    Add examples and explanations for enums in ArrayEnumTuples.ts

--- TYPESCRIPT/basic/src/ArrayEnumTuples.ts
+++ TYPESCRIPT/basic/src/ArrayEnumTuples.ts
@@ -28,3 +28,63 @@ let user: [string, number, boolean];
 
 user = ["Riya", 25, true]; // ✅ correct
 // user = ["Riya", true, 25]; // ❌ wrong order
+
+// Order matter karta hai
+// Length bhi fixed hoti hai
+
+// --------------------------------------------------------------------------------------------------------------------------------
+
+// 3. Enum in TypeScript
+
+// Syntax:
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+// Example:
+enum Status {
+  Success,
+  Error,
+  Loading,
+}
+
+let currentStatus: Status = Status.Success;
+console.log(currentStatus); // 0
+
+// Custom values:
+enum Role {
+  Admin = "ADMIN",
+  User = "USER",
+  Guest = "GUEST",
+}
+
+let myRole: Role = Role.Admin;
+
+// Default enum numbers se start hota hai (0,1,2…)
+// Tum custom values bhi de sakte ho (string ya number)
+
+// Quick Comparison :-
+// | Feature  | Array              | Tuple               | Enum               |
+// | -------- | ------------------ | ------------------- | ------------------ |
+// | Type     | Same type elements | Fixed types + order | Fixed named values |
+// | Length   | Flexible           | Fixed               | Fixed              |
+// | Use case | List of items      | Structured data     | Constants          |
+
+// Real-life Example:
+// Array
+let marks: number[] = [90, 85, 88];
+
+// Tuple
+let student: [string, number] = ["Aman", 90];
+
+// Enum
+enum Grade {
+  A = "Excellent",
+  B = "Good",
+  C = "Average",
+}
+
+let result: Grade = Grade.A;
