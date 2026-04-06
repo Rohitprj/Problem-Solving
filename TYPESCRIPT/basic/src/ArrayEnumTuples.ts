3d3c4d43576376dbc8cd0ddee7ec780d26964ce6
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-06 18:20:02 +0530

    Add ArrayEnumTuples.ts to demonstrate array and tuple usage in TypeScript

---
+++ TYPESCRIPT/basic/src/ArrayEnumTuples.ts
@@ -0,0 +1,8 @@
+let numbers: number[] = [1, 2, 3, 4];
+
+let numbers1: Array<number> = [1, 2, 3];
+
+let names: string[] = ["Aman", "Riya", "Rahul"];
+
+names.push("Neha"); // valid
+// names.push(10); // ❌ error (kyunki number allowed nahi hai)
