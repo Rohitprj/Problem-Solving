c8cc2f06028a98321d0b50f142341e3f8981177d
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-11 16:25:20 +0530

    Refactor project structure: update index.html title, comment out unused CSS, and create FirstComp component

---
+++ FirstReactApp/src/components/FirstComp.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function FirstComp() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+        architecto tempore beatae illo tempora incidunt, ipsam non quae,
+        repellat voluptas ad doloremque, nemo autem magni eos enim tenetur iusto
+        molestiae commodi aut aliquid ut sed corrupti! Earum ipsa, quaerat
+        dolorum modi natus impedit blanditiis, dicta praesentium asperiores quas
+        quod quis.
+      </p>
+      <h1>Hello</h1>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+      <h2>World</h2>
+    </div>
+  );
+}
+
+export default FirstComp;
