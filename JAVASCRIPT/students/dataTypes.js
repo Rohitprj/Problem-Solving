63de8793f3ae402d4ec839a024a735d643e20365
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-14 13:39:46 +0530

    Add eComData.json with customer orders and inventory details

---
+++ JAVASCRIPT/students/dataTypes.js
@@ -0,0 +1,39 @@
+// let student = {
+//   name: "Nitin",
+//   course: "BCA",
+//   marks: 70,
+//   isPassed: true,
+//   isCompleted: null,
+//   books: {
+//     sem1: "Yes",
+//     isClear: false,
+//     mca: {
+//       isMCAStu: false,
+//       course: "MCA",
+//     },
+//   },
+// };
+
+// console.log(Object.values(student));
+
+// let value = [1, 2, 3, 4, 5];
+
+// let a = [
+//   1,
+//   "two",
+//   { name: "MERN", course: "BCA", marks: [1, 2, 3] },
+//   [3, 4, 5],
+// ];
+
+// console.log(a[2]);
+
+// let a;
+
+// a = 10;
+// console.log(a);
+
+// a = "Nitin";
+// console.log(a);
+
+const b = null;
+console.log(b);
