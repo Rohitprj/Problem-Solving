23e4b276ee7dd4e04f0892b6eafa43beeabafeda
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-15 14:56:13 +0530

    Add scopes.js with variable declarations and console logs for demonstration

---
+++ JAVASCRIPT/students/scopes.js
@@ -0,0 +1,33 @@
+// let a = "Hello world";
+
+// {
+//   console.log(a);
+// }
+
+// function hi() {
+//   console.log(a);
+// }
+// hi();
+
+// console.log(a);
+
+// function scope() {
+//   var a = "Hello";
+//   let b = "world";
+//   const c = "Hello world";
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// }
+
+// scope();
+// console.log(a, b, c);
+
+{
+  var name = "Rohit";
+  let age = 1;
+  const add = "Delhi";
+  console.log(name, age, add);
+}
+
+console.log(name);
