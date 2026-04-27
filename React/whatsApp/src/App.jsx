3ce58ba9342ff8980860d9e0c306918a2a1794b0
Rohit kumar <rohitprj.1282@gmail.com>
2026-04-27 13:23:03 +0530

    chatApp

---
+++ React/whatsApp/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import WhatsApp from "./WhatsApp";
+
+export default function App() {
+  return (
+    <div>
+      <WhatsApp />
+    </div>
+  );
+}
---
+++ React/whatsApp/src/WhatsApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WhatsApp() {
+  return (
+    <div>
+      <p>WhatsApp</p>
+    </div>
+  );
+}
