898555718711d4f13a1f3b0802e439e58b139edf
Rohit kumar <rohitprj.1282@gmail.com>
2026-05-05 16:00:17 +0530

    feat: add SVG assets for React and Vite, create context provider, and set up Vite configuration with Tailwind CSS

---
+++ React/contextApi/src/context/Context.jsx
@@ -0,0 +1,12 @@
+import React, { createContext } from "react";
+
+export const Context = createContext();
+
+const Context = (props) => {
+  return 
+    <Context.Provider value={}> 
+        {props.children}
+    </Context.Provider>  
+};
+
+export default context;
