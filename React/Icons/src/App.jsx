a58f94ca0149a4cf619d1a8febcb4cfd92c2245c
Rohit kumar <rohitprj.1282@gmail.com>
2026-05-14 13:30:48 +0530

    feat: integrate Tailwind CSS and update dependencies
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated package.json and package-lock.json to include Tailwind CSS dependencies.
    - Refactored App component to utilize Tailwind CSS for styling.
    - Created a new Amin component for loading animations.
    - Updated index.css to import Tailwind CSS and commented out default styles.
    - Adjusted vite.config.js to include Tailwind CSS plugin.

--- React/Icons/src/App.jsx
+++ React/Icons/src/App.jsx
@@ -124,24 +124,35 @@ import React from "react";
 import { IoMdSettings } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
 import Profile from "./components/Profile";
+import { FaArrowDown } from "react-icons/fa";
+import Amin from "./components/Amin";
 
 export default function App() {
   return (
     <>
-      <div
-        style={{
-          padding: "40px",
-          backgroundColor: "lightblue",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <CgProfile size={50} color="green" />
-
-        <Profile dev="Developer" name="Rohit" />
-
-        <IoMdSettings size={50} color="red" />
+      <div class="mx-auto w-full max-w-sm rounded-md border border-blue-300 p-4">
+        <div class="flex animate-pulse space-x-4">
+          <div class="size-10 rounded-full bg-gray-200"></div>
+          <div class="flex-1 space-y-6 py-1">
+            <div class="h-2 rounded bg-gray-200"></div>
+            <div class="space-y-3">
+              <div class="grid grid-cols-3 gap-4">
+                <div class="col-span-2 h-2 rounded bg-gray-200"></div>
+                <div class="col-span-1 h-2 rounded bg-gray-200"></div>
+              </div>
+              <div class="h-2 rounded bg-gray-200"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
+}
+
+{
+  /* <CgProfile size={50} color="green" />
+
+<Profile dev="Developer" name="Rohit" />
+
+<IoMdSettings size={50} color="red" /> */
 }
