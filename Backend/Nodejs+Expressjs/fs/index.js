ce2ff8f3387d387eeb69f89420500ea116767871
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-19 16:12:12 +0530

    feat: add file system examples and initial setup for Node.js project

---
+++ Backend/Nodejs+Expressjs/fs/index.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+// console.log(fs);
+
+// console.log("Starts");
+// fs.writeFileSync("notes.js", "console.log('hello world')", () =>
+//   console.log("Executing"),
+// );
+// console.log("Ends");
+
+// console.log("Starts");
+// fs.writeFile("new.txt", "Hello i am web developer", () =>
+//   console.log("Executing"),
+// );
+// console.log("Ends");
+
+// const data = fs.readFileSync("new.txt", "utf8");
+// console.log(data);
+
+// fs.appendFileSync("new.txt", "\n and i am a java developer");
