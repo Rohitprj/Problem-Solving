4c88fd700211cd2eb2ebb78a5054b8f691e3fdd8
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-19 17:22:05 +0530

    feat: refactor index.js to uncomment server setup and update package.json scripts

--- Backend/Nodejs+Expressjs/nodejs/index.js
+++ Backend/Nodejs+Expressjs/nodejs/index.js
@@ -1,4 +1,19 @@
 // const cat = require("cat-me");
-import cat from "cat-me";
+// import cat from "cat-me";
 
-console.log(cat());
+// console.log(cat());
+
+import { createServer } from "node:http";
+
+const hostname = "127.0.0.1";
+const port = 3001;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello my name is Rohit");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
