c5fd8a26be4b8cfe46853be20acfdeb07698e6d3
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-21 13:47:44 +0530

    feat: add initial files for file system operations and project structure

---
+++ Backend/students/fs/index.js
@@ -0,0 +1,19 @@
+// const fs = require("fs");
+import fs from "fs";
+
+// console.log("start");
+// fs.writeFileSync("notes1.txt", "Hello Students");
+// console.log("end");
+
+// console.log("start");
+// fs.writeFile("student.txt", "Hello i am student", () => {
+//   console.log("Executing");
+// });
+// console.log("end");
+
+// const data = fs.readFileSync("student.txt", "utf8");
+// console.log(data);
+
+// fs.appendFileSync("student.txt", " and i am a java developer");
+
+// fs.renameSync("student.txt", "class.txt");
