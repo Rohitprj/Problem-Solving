5ea9aa30272d2d64a9ab0b1d04f7a502182f5997
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-29 17:14:57 +0530

    feat: enhance form handling in App component with state management

--- React/FormHandling/src/App.jsx
+++ React/FormHandling/src/App.jsx
@@ -1,18 +1,47 @@
-import React from "react";
+// import React from "react";
+// import "./App.css";
+
+// function App() {
+//   function form(e) {
+//     e.preventDefault();
+//     console.log("form submitted");
+//   }
+//   return (
+//     <>
+//       {/* <p>React</p> */}
+//       <form onSubmit={(e) => form(e)}>
+//         <input type="text" placeholder="Email" />
+//         <br />
+//         <input type="text" placeholder="Password" />
+//         <br />
+//         <button>Login</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+
   function form(e) {
     e.preventDefault();
-    console.log("form submitted");
+    console.log("form submitted by", value);
+    setValue("");
   }
   return (
     <>
-      {/* <p>React</p> */}
       <form onSubmit={(e) => form(e)}>
-        <input type="text" placeholder="Email" />
-        <br />
-        <input type="text" placeholder="Password" />
+        <input
+          type="text"
+          placeholder="enter your name"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
         <br />
         <button>Login</button>
       </form>
