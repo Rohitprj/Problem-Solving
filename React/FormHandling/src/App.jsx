f3b8f5698667b1547e0df2dbda07d62bd453d5b7
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-29 18:45:09 +0530

    feat: update form component with label and improved logging

--- React/FormHandling/src/App.jsx
+++ React/FormHandling/src/App.jsx
@@ -22,20 +22,50 @@
 
 // export default App;
 
+// import React, { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [value, setValue] = useState("");
+
+//   function form(e) {
+//     e.preventDefault();
+//     console.log("form submitted by", value);
+//     setValue("");
+//   }
+//   return (
+//     <>
+//       <form onSubmit={(e) => form(e)}>
+//         <input
+//           type="text"
+//           placeholder="enter your name"
+//           value={value}
+//           onChange={(e) => setValue(e.target.value)}
+//         />
+//         <br />
+//         <button>Login</button>
+//       </form>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import "./App.css";
 
 function App() {
   const [value, setValue] = useState("");
-
   function form(e) {
     e.preventDefault();
-    console.log("form submitted by", value);
+    console.log("Form submitted", value);
     setValue("");
   }
   return (
     <>
       <form onSubmit={(e) => form(e)}>
+        <label>Name</label>
+        <br />
         <input
           type="text"
           placeholder="enter your name"
@@ -43,7 +73,7 @@ function App() {
           onChange={(e) => setValue(e.target.value)}
         />
         <br />
-        <button>Login</button>
+        <button>submit</button>
       </form>
     </>
   );
