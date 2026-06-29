a1086cef83b2341f721b105b4e5d62f4f3c9a235
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-29 13:55:27 +0530

    feat: add axios type definitions and enhance web request error handling

--- TYPESCRIPT/basic/src/webRequest.ts
+++ TYPESCRIPT/basic/src/webRequest.ts
@@ -1,3 +1,36 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-axios;
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const getData = async () => {
+  try {
+    const response: AxiosResponse<Todo> = axios.get(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+
+    console.log(response.data);
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      console.log("Axios Error", error.message);
+      if (error.response) {
+        console.log(error.response.status);
+      }
+    }
+  }
+};
+
+// axios.get<Todo>("https://jsonplaceholder.typicode.com/todos/1").then((res) => {
+//   console.log(res.data);
+// });
+
+// {
+//   "userId": 1,
+//   "id": 1,
+//   "title": "delectus aut autem",
+//   "completed": false
+// }
