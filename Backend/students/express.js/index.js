ea33319987e2d5d1a0d4bb3998b1a0822c3ffc68
Rohit kumar <rohitprj.1282@gmail.com>
2026-06-30 18:17:45 +0530

    feat: implement basic note-taking functionality with GET and POST endpoints

--- Backend/students/express.js/index.js
+++ Backend/students/express.js/index.js
@@ -2,10 +2,25 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3001;
+
+const getNotes = [];
 
 app.get("/my-self", (req, res) => {
   res.send("My name is Rohit Kumar");
+});
+
+app.post("/notes", (req, res) => {
+  const data = req.body;
+  getNotes.push(data);
+  console.log(data);
+  res.status(200).send("Note Created Successfully");
+});
+
+app.get("/notes", (req, res) => {
+  res.send(getNotes);
 });
 
 app.listen(PORT, () => {
