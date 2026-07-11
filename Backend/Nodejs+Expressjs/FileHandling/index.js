53d1d6f8685c4566a4aaa731184c7e6e929de52c
Manish Kumar <rohitprj.1282@gmail.com>
2026-07-11 17:59:24 +0530

    feat: add multer for file upload handling and update package.json

--- Backend/Nodejs+Expressjs/FileHandling/index.js
+++ Backend/Nodejs+Expressjs/FileHandling/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import multer from "multer";
 
 dotenv.config();
-
+const upload = multer({ storage:multer.memoryStorage() });
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
+
+app.post("/upload", upload.single("Image"), (req, res) => {
+console.log(req.files);
+console.log(req.body);
+res.json({ message: "File uploaded successfully!" });
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
