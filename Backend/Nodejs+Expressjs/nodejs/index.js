3580da36e4c21fc3c4087c6dfc98afb4b89725a7
Rohitprj <rohitprj.1282@gmail.com>
2026-08-26 15:21:52 +0530

    feat: update server response to return HTML content and change status code

--- Backend/Nodejs+Expressjs/nodejs/index.js
+++ Backend/Nodejs+Expressjs/nodejs/index.js
@@ -9,11 +9,11 @@ const hostname = "127.0.0.1";
 const port = 3001;
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello my name is Rohit");
+  res.statusCode = 201;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello my name is Rohit</h1><p>I am a Node.js developer.</p>");
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/my-self`);
 });
